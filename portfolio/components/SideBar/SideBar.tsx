1cf349a7daaadd799f5ea517f5e2f54df4553b9c
ramin709 <79803446+ramin709@users.noreply.github.com>
2022-06-26 12:29:27 +0430

    complete SideBar component
    SideBar component completed and background color of main app changed.

--- portfolio/components/SideBar/SideBar.tsx
+++ portfolio/components/SideBar/SideBar.tsx
@@ -1,23 +1,48 @@
 import React from 'react'
 import style from './SideBar.module.css'
 import Image from 'next/image'
+import { AiFillLinkedin, AiFillYoutube, AiFillGithub } from 'react-icons/ai'
+import { GoLocation } from 'react-icons/go'
+import { GiTie } from 'react-icons/gi'
 
 const SideBar = () => {
   return (
     <div className={style.container}>
-      <div className={style.img}>
-        <Image src="/avatar.jpg" alt="avatar" width={300} height={300}  ></Image>
+      <div className={style.imgContainer}>
+        <Image src="/avatar.jpg" alt="avatar" width={150} height={150} className={style.image}></Image>
       </div>
-      <h3 className="name">
-        <span className="firstName">John</span>
+      <h3 className={style.name}>
+        <span className={style.firstName}>John </span>
         Doe
       </h3>
 
-      <p className="title">Web developer</p>
-      <p className="title">Download Resume</p>
+      <p className={style.title}>Web developer</p>
+
+
+      <div className={style.resume}>
+        <GiTie className={style.icons} />
+        <a href="" className={style.resumeLink}>Download Resume</a>
+      </div>
+
+      <div className={style.socialMediaLinks}>
+        <a href="">
+          <AiFillGithub className={style.socialMediaIcons} />
+        </a>
+
+        <a href="">
+          <AiFillLinkedin className={style.socialMediaIcons} />
+        </a>
+
+        <a href="">
+          <AiFillYoutube className={style.socialMediaIcons} />
+        </a>
+      </div>
 
       <div className={style.contactInfo}>
-        <span className={style.info}>Berlin , Germany</span>
+        <div className={style.location}>
+          <GoLocation className={style.icons} />
+          <span className={style.info}>Berlin , Germany</span>
+        </div>
         <span className={style.info}>JohnDoe@gmail.com</span>
         <span className={style.info}>87392133 / 32532312</span>
       </div>
